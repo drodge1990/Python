58101be526c2ab58a055a160d1a37373c246a64c
drodge1990 <74329964+drodge1990@users.noreply.github.com>
2025-01-30 12:21:36 +0000

    update

--- fileManipulation/CCExtract.py
+++ fileManipulation/CCExtract.py
@@ -27,12 +27,13 @@ for x in ccOut:
         ccImp['<EMPID>'] = x['Payroll Number']
         ccImp['<1120>'] = x['Hours']
         ccImp['<1120R>'] = x['Rate']
-    # elif x['Type'] in {'Bank Holiday','Special Day 1','Special Day 2'}:
-    #     ccImp['<1250>'] = x['Hours']
-    #     ccImp['<1250R>'] = x['Rate']
-    # elif x['Type'] in {'Holiday', 'Saturday Holiday', 'Sunday Holiday'}:
-    #     ccImp['<1440>'] = x['Hours']
-    #     ccImp['<1440R>'] = x['Rate']
-fields = ['<EMPID>','<POSTID>','<1120>','<1120R>','<1121>','<1121R>','<1122>','<1122R>','<1250>','<1250R>','<1440>','<1440R>']
+    elif x['Type'] in {'Bank Holiday','Special Day 1','Special Day 2'}:
+        ccImp['<1250>'] = x['Hours']
+        ccImp['<1250R>'] = x['Rate']
+    elif x['Type'] in {'Holiday', 'Saturday Holiday', 'Sunday Holiday'}:
+        ccImp['<1440>'] = x['Hours']
+        ccImp['<1440R>'] = x['Rate']
+
+fields = ['<EMPID>','<POSTID>':'MAIN','<1120>','<1120R>','<1121>','<1121R>','<1122>','<1122R>','<1250>','<1250R>','<1440>','<1440R>']
 dictOutput = csv.DictWriter(open('./fileManipulation/output.csv','w'), fieldnames=fields)
 dictOutput.writerows(ccImp)
