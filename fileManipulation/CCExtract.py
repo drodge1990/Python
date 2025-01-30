5205c4c94f9bfe6f5bb8fda4c7538a1d4fff0784
drodge1990 <74329964+drodge1990@users.noreply.github.com>
2025-01-30 12:12:43 +0000

    Changed to use csv module

--- fileManipulation/CCExtract.py
+++ fileManipulation/CCExtract.py
@@ -1,35 +1,38 @@
-with open('./fileManipulation/input.txt','r') as file:
-    list1 = []
-    list2 = []
+# with open('./fileManipulation/input.txt','r') as file:
+#     list1 = []
+#     list2 = []
 
-    for line in file:
-        line = line.strip()
+#     for line in file:
+#         line = line.strip()
+import csv
 
+ccOut = csv.DictReader(open('./fileManipulation/input.csv','r'))
 
 ccImp = []
 
-ee = []
+a = {'<EMPID>':None,'<POSTID>':'MAIN','<1120>':None,'<1120R>':None,'<1121>':None,'<1121R>':None,'<1122>':None,'<1122R>':None,'<1250>':None,'<1250R>':None,'<1440>':None,'<1440R>':None}
 
-for line in list1:
-    ee = list1[0]
-a = {'<POSTID>':'MAIN','<1120>':None,'<1120R>':None,'<1121>':None,'<1121R>':None,'<1122>':None,'<1122R>':None,'<1250>':None,'<1250R>':None,'<1440>':None,'<1440R>':None}
-ee = [*{*ee}]
-ccImp = {EMPID: a for EMPID in ee}
 
-for x in list1:
-    currEmp = next(item for item in ccImp if item['EMPID'] == x[0])
-    if x[8] == 'Standard':
-        currEmp['<1120>'] = x[9]
-        currEmp['<1120R>'] = x[10]
-    elif x[8] == 'Saturday':
-        currEmp['<1121>'] = x[9]
-        currEmp['<1121R>'] = x[10]
-    elif x[8] == 'Sunday':
-        currEmp['<1120>'] = x[9]
-        currEmp['<1120R>'] = x[10]
-    # elif x[8] in {'Bank Holiday','Special Day 1','Special Day 2'}:
-    #     currEmp['<1250>'] = x[9]
-    #     currEmp['<1250R>'] = x[10]
-    # elif x[8] in {'Holiday', 'Saturday Holiday', 'Sunday Holiday'}:
-    #     currEmp['<1440>'] = x[9]
-    #     currEmp['<1440R>'] = x[10]
+for x in ccOut:
+
+    if x['Type'] == 'Standard':
+        ccImp['<EMPID>'] = x['Payroll Number']
+        ccImp['<1120>'] = x['Hours']
+        ccImp['<1120R>'] = x['Rate']
+    elif x['Type'] == 'Saturday':
+        ccImp['<EMPID>'] = x['Payroll Number']
+        ccImp['<1121>'] = x['Hours']
+        ccImp['<1121R>'] = x['Rate']
+    elif x['Type'] == 'Sunday':
+        ccImp['<EMPID>'] = x['Payroll Number']
+        ccImp['<1120>'] = x['Hours']
+        ccImp['<1120R>'] = x['Rate']
+    # elif x['Type'] in {'Bank Holiday','Special Day 1','Special Day 2'}:
+    #     ccImp['<1250>'] = x['Hours']
+    #     ccImp['<1250R>'] = x['Rate']
+    # elif x['Type'] in {'Holiday', 'Saturday Holiday', 'Sunday Holiday'}:
+    #     ccImp['<1440>'] = x['Hours']
+    #     ccImp['<1440R>'] = x['Rate']
+fields = ['<EMPID>','<POSTID>','<1120>','<1120R>','<1121>','<1121R>','<1122>','<1122R>','<1250>','<1250R>','<1440>','<1440R>']
+dictOutput = csv.DictWriter(open('./fileManipulation/output.csv','w'), fieldnames=fields)
+dictOutput.writerows(ccImp)
