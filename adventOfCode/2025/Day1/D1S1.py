a4a46e5e8a287ae0ea6fcdb4f9b98edb7f9ccd59
drodge <drodge1990@hotmail.com>
2026-01-09 14:44:15 +0000

    test1

--- adventOfCode/2025/Day1/D1S1.py
+++ adventOfCode/2025/Day1/D1S1.py
@@ -3,10 +3,25 @@ if test:
     source = './adventOfCode/2025/Day1/testInputCodeD1.txt'
 else:
     source = './adventOfCode/2025/Day1/inputCodeD1.txt'
+list1 = []
 
 with open(source,'r') as file:
-    list1 = []
-
     for line in file:
         line = line.strip()
         list1.append(line)
+
+direction = ''
+value = 0
+currentPosition = 0
+
+for x in list1:
+    direction = ''.join(char for char in x if char.isalpha())
+    value = int(''.join(char for char in x if char.isnum()))
+    if direction is 'L':
+        value = value * (-1)
+    
+    currentPosition = currentPosition+value
+    if currentPosition < 0:
+        currentPosition + 100
+    elif currentPosition > 99:
+        currentPosition -100
