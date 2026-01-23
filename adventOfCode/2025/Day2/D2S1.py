6bd7797847f7b457f2008aaaed3263051024df47
drodge <drodge1990@hotmail.com>
2026-01-23 23:27:09 +0000

    day 2

--- adventOfCode/2025/Day2/D2S1.py
+++ adventOfCode/2025/Day2/D2S1.py
@@ -1,7 +1,27 @@
+import math
+import textwrap
+
+def invalid(id):
+
+    lengthId = math.floor(math.log10(abs(id))) + 1
+    #mid = int(lengthId/2)
+    idStr = str(id)
+
+    for splitValue in range(lengthId,1,-1):
+        if (lengthId/splitValue) % 1 == 0:
+            chunks = textwrap.wrap(idStr,int(lengthId/splitValue))
+            if len(set(chunks)) == 1:
+                return id
+
+    #if idStr[0:(mid)] == idStr[mid:]:
+        #return id
+    
+    return 0
+
 def validCheck(current):
     
-    if current % 2 != 0:
-        current
+    if (math.floor(math.log10(abs(current))) + 1) % 2 == 0:
+        current = invalid(current)
     else:
         current = 0
 
@@ -14,7 +34,6 @@ def readFile(filename):
     with open(filename,'r') as file:
         for line in file:
             line = line.strip()
-            line = line.split(',')
             list1.append(line)
 
     return list1
@@ -22,24 +41,23 @@ def readFile(filename):
 
 if __name__ == "__main__":
 
-    source = './adventOfCode/2025/Day2/testInputCodeD2.txt'
-    #source = './adventOfCode/2025/Day2/InputCodeD2.txt'
-
-    invalidID = []
+    #source = './adventOfCode/2025/Day2/testInputCodeD2.txt'
+    source = './adventOfCode/2025/Day2/InputCodeD2.txt'
+    invalidId = []
     list1 = readFile(source)
 
-    for range in list1:
-        range.split()
-        start = range[0]
-        end = range[-1]
+    for line in list1:
+        rang = line.split(',')
+        for pair in rang:
+            
+            item = pair.split('-')
+            start = int(item[0])
+            end = int(item[-1])
 
-        ID = start
-        
-        while ID < end:
-        
-            invalidID.append(validCheck(ID))
-            ID += 1
+            for id in range(start,end+1):
+                #print (id)
+                invalidId.append(invalid(id))
 
-    print(sum(invalidID))
+    print(sum(invalidId))
 
 
