bf2d4681c7fa0f510c679b982da77df26f2d7c80
drodge <drodge1990@hotmail.com>
2026-02-18 14:56:54 +0000

    Day7 part2 working

--- adventOfCode/2025/Day7/D7.py
+++ adventOfCode/2025/Day7/D7.py
@@ -4,44 +4,43 @@ def read_file(source):
 
     content = content.splitlines()
     content = [list(x) for x in content]
-
+    
+    for line in content:
+        for i,item in enumerate(line):
+            if item == '.':
+                line[i] = 0
+    
     return content
 
 def splitting(dataset,start):
     
     interim = [start.index('S')]
-    start[interim[0]] = '|'
+    start[interim[0]] = 1
     output = start
     count = 0
-    #part2 = 0
-    #prev_interim = 1
-
+    
     for line in dataset:
-        #part2_interim = 0
         positions = [index for index, value in enumerate(line) if value == '^']
         try:
             for value in positions:
                 if value in interim:
-                    #part2_interim += 1
                     count += 1
-                    output[value] = '.'
-                    output[value - 1],output[value +1] = '|','|'
-                    interim = [index for index, pos1 in enumerate(output) if pos1 == '|']
+                    output[value - 1] += output[value]
+                    output[value +1] += output[value]
+                    output[value] = 0
+                    interim = [index for index, pos1 in enumerate(output) if pos1 > 0]
         except Exception as error:
             print(error)
             pass
 
-        #part2 += n
-        #prev_interim = part2_interim
-
-    return count#,part2
+    return count,output
 
 if __name__ == '__main__':
 
     test = './adventOfCode/2025/Day7/test.txt'
     source = './adventOfCode/2025/Day7/source.txt'
 
-    dataset = read_file(test)
+    dataset = read_file(source)
     start = dataset.pop(0)
     output = splitting(dataset,start)
 
